d4248b1da10c4b8e9bb85c85f48acfc3c28bee4d
Richard Coney <richardconey46@gmail.com>
2025-11-24 21:06:47 -0500

    Attempted to display API facts

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const apiKey = "live_ByBO5K46WgYYzRqR2fklSSiptGh0HLX1w4BjlwSKHgDIvnx4t9enTHVpacmkqxb3";
 const fact = document.getElementById("cat-fact");
+const url = "https://api.thecatapi.com/v1/facts";
+
+const response = fetch(url, {headers: {"x-api-key": apiKey}})
+ .then(res => res.json())
+  .then(data => console.log(data[0].fact));
