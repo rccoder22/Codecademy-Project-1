ca532bb03fe61f3ebcc7ccad960d6df7798417fb
Richard Coney <richardconey46@gmail.com>
2025-11-24 21:11:31 -0500

    Cat API facts successful

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const apiKey = "live_ByBO5K46WgYYzRqR2fklSSiptGh0HLX1w4BjlwSKHgDIvnx4t9enTHVpacmkqxb3";
 const fact = document.getElementById("cat-fact");
-const url = "https://api.thecatapi.com/v1/facts";
+const url = "https://catfact.ninja/fact";
 
-const response = fetch(url, {headers: {"x-api-key": apiKey}})
+const response = fetch(url)
  .then(res => res.json())
-  .then(data => console.log(data[0].fact));
+  .then(data => console.log(data.fact));
