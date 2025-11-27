922f218b8eb18f465eb63f395058045309beb59a
Richard Coney <richardconey46@gmail.com>
2025-11-26 21:09:29 -0500

    Updated console.log and images.src to get diff images on each click

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const pixabayApiKey = "53427526-fe30e2c0c76c912f8b2e0f609";
 const fact = document.getElementById("cat-fact");
 const url = "https://catfact.ninja/fact";
 const button = document.getElementById("cat-fact-btn");
-// const pixabayURL = "https://pixabay.com/api/";
 const image = document.getElementById("cat-photo");
 const pixabayURL = `https://pixabay.com/api/?key=${pixabayApiKey}&q=cat&image_type=photo`;
 
@@ -16,8 +15,8 @@ function getFact() {
     fetch(pixabayURL)
         .then(res => res.json())
         .then(data =>
-            {console.log(data.hits[0]);
-            image.src = data.hits[0].webformatURL;
+            {console.log(data.hits[Math.floor(Math.random() * data.hits.length)]);
+            image.src = data.hits[Math.floor(Math.random() * data.hits.length)].webformatURL;
     });
 }
 
