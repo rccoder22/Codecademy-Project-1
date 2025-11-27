f324f2b6f60c3a6f052091d43468653a9e4f31f7
Richard Coney <richardconey46@gmail.com>
2025-11-26 20:46:34 -0500

    Added Image API key and Pixabay URL

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-const apiKey = "live_ByBO5K46WgYYzRqR2fklSSiptGh0HLX1w4BjlwSKHgDIvnx4t9enTHVpacmkqxb3";
+const pixabayApiKey = "53427526-fe30e2c0c76c912f8b2e0f609";
 const fact = document.getElementById("cat-fact");
 const url = "https://catfact.ninja/fact";
 const button = document.getElementById("cat-fact-btn");
+// const pixabayURL = "https://pixabay.com/api/";
+const image = document.getElementById("cat-image");
+const pixabayURL = `https://pixabay.com/api/?key=${pixabayApiKey}&q=cat&image_type=photo`;
+
 function getFact() {
-    const response = fetch(url)
-   .then(res => res.json())
-   .then(data => 
-    {console.log(data.fact);
-        fact.textContent = data.fact;
+    fetch(url)
+        .then(res => res.json())
+        .then(data =>
+            {console.log(data.fact);
+            fact.textContent = data.fact;
+    });
+    fetch(pixabayURL)
+        .then(res => res.json())
+        .then(data =>
+            {console.log(data.hits[0]);
+                console.log(pixabayURL);
+            image.src = data.image;
     });
 }
+
 
 button.addEventListener('click', getFact);
